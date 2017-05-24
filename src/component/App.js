bb710fd96b26a241455b7e07270e9c0e9373cf2b
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2017-05-24 15:40:01 +1000

    modified react component as presentational component

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,9 @@
 import React from 'react' ;
 import '../styles/src.scss';
 
-export default class App extends React.Component {
-	
-	render(){
-		return (
-		<div className="my-test-class"> I am from react component</div>
-		)
+	export default function App (){
+	return( <div className='my-test-class'> I am from react component</div>)
 	}
-}
+
+
+	
